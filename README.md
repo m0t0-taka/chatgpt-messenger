ca515ff8ee437477e601f49092244e6643f87fbd
m0t0-taka <tkg.8.mthr@gmail.com>
2023-02-10 22:33:28 +0900

    Home Page 作成

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 `npx create-next-app -e with-tailwindcss chatgpt-messenger`
 
+## 使用技術
+
+- UI
+  - tailwindCSS
+  - Heroicons
+
 ## メモ
 
 - app directory を使用。default の index.tsx は不要のため削除。app directory を追加して`npm run dev`を実行すると、head, layout が作成される
+
+- カスタム CSS を global.css に作成
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,59 @@
+import {
+  BoltIcon,
+  ExclamationTriangleIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>ChatGPT Messenger</h1>
+    <div className="flex flex-col items-center justify-center h-screen text-white">
+      <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+
+      <div className="flex space-x-2 text-center">
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <SunIcon className="h-8 w-8" />
+            <h2>例</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">"何か説明して"</p>
+            <p className="infoText">"犬と猫の違いは何？"</p>
+            <p className="infoText">"太陽の色は何色？"</p>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <BoltIcon className="h-8 w-8" />
+            <h2>能力</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">
+              ChatGPT Modalを使用できるよう変更してください
+            </p>
+            <p className="infoText">
+              検索文はFirebaseのFirestoreに保存されます
+            </p>
+            <p className="infoText">
+              ChatGPTが考えている間にトーストのお知らせあるよ
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5">
+            <ExclamationTriangleIcon className="h-8 w-8" />
+            <h2>制限</h2>
+          </div>
+          <div className="space-y-2">
+            <p className="infoText">正しくない答えが返ってくることもあるよ</p>
+            <p className="infoText">気分を害すことを言うこともあるよ</p>
+            <p className="infoText">
+              2021年以降のイベントや世界のことは知らないよ
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
