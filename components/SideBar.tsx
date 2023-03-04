7912390cb57e35727438283f61172b046a3f987f
m0t0-taka <tkg.8.mthr@gmail.com>
2023-03-04 23:17:23 +0900

    ログアウト追加

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 const SideBar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -11,6 +15,14 @@ const SideBar = () => {
           </div>
         </div>
       </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   );
 };
